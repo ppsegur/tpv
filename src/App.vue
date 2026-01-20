171fc2d49af176a7b816f43afff2c4247f593f2c
José Rafael <114216867+ppsegur@users.noreply.github.com>
2026-01-20 08:37:54 +0100

    Fix navbar rendering and mobile toggle

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,29 @@
 <template>
-  <NavBar />
-  <router-view />
+  <div class="container">
+    <NavBar 
+      :aria-expanded="menuOpen"
+      @click="menuOpen = !menuOpen"
+      :class="{ open: menuOpen }">
+    </NavBar>
+    <router-view />
+  </div>
 </template>
 
-<script>
-export default {
-  name: 'App'
-};
+<script setup>
+import NavBar from './components/Nav-bar.vue';
+
+const menuOpen = ref(false);
 </script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 16px;
+}
+
+.open {
+  /* Add your responsive styles here */
+}
+</style>
